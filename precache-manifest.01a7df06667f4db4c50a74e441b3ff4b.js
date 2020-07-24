400728873737a2dd9a90ddf4091e44abf9d658b0
saadgitt <saad.akbar@tintash.com>
2020-07-24 20:59:28 +0500

    Updates

---
+++ precache-manifest.01a7df06667f4db4c50a74e441b3ff4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aac34cad6ebf04434874bfaae525421",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63fde5154450e9a5ef22",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a58202dbaa3870363437",
+    "url": "/static/js/2.dc68b0dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dc68b0dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63fde5154450e9a5ef22",
+    "url": "/static/js/main.e1074290.chunk.js"
+  },
+  {
+    "revision": "405f430893446efaf6a2",
+    "url": "/static/js/runtime-main.9129b20f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
